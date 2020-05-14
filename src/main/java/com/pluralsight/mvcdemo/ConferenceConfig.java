1956606dab74262d8600f877df79b49b93e77337
Govardhan <govardhan.yannam@gmail.com>
2020-05-14 13:45:57 +0530

    thymeleaf configuration and example

--- src/main/java/com/pluralsight/mvcdemo/ConferenceConfig.java
+++ src/main/java/com/pluralsight/mvcdemo/ConferenceConfig.java
@@ -1,5 +1,7 @@
 package com.pluralsight.mvcdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -10,11 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import java.util.Locale;
 
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
+
+    private ApplicationContext applicationContext;
 
     /*
     Override this method to configure static pages resources location
@@ -48,7 +55,37 @@ public class ConferenceConfig implements WebMvcConfigurer {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
-        viewResolver.setOrder(0);
+        viewResolver.setOrder(1);
         return viewResolver;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver resourceTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    @Autowired
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine()  {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(resourceTemplateResolver());
+        engine.setEnableSpringELCompiler(true);
+        return engine;
+    }
+
+    @Bean
+    public ViewResolver thymeLeafResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setOrder(0);
+        return resolver;
     }
 }
